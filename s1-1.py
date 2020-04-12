fb9ccee5fc7a9767d14508ac140496e7e9bdaac6
allenz16 <36390397+allenz16@users.noreply.github.com>
2020-04-12 15:36:59 +0800

    created the first python file s1-1.py

---
+++ s1-1.py
@@ -0,0 +1,29 @@
+# expressions = values + operators
+2 + 2
+2
+5 - 3
+3 * 7
+22 / 7
+2 + 3 * 6
+(2 + 3) * 6
+(5 - 1) * ((7 + 1) / (3 - 1))
+'Alice' + 'Bob'
+'Alice' * 3
+'Hello' + '!' * 10
+
+# int
+-2
+30
+
+# float
+3.14
+42.0
+
+# string
+'Hello world'
+
+# variable
+spam = 42
+spam = 2 + 2
+spam = spam + 1
+
