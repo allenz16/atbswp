11cfb540b05afe82b5a1faf44d6ee5248f50b17a
allenz16 <36390397+allenz16@users.noreply.github.com>
2020-04-12 17:13:42 +0800

    created s1-2.py

---
+++ s1-2.py
@@ -0,0 +1,9 @@
+# This program says hello and asks for my name and age.
+print('Hello world.')
+print('What\'s your name?')
+myName = input()
+print('Nice to meet you ' + myName + '.')
+print('The length of your name is ' + str(len(myName)) + '.')
+print('What\'s your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
