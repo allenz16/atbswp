a248448b18bf9ab728df65bab7700a8a7f7e85d4
allenz16 <36390397+allenz16@users.noreply.github.com>
2020-04-13 10:25:02 +0800

    created s2-1,2,3,4,5,6.py

---
+++ s2-3.py
@@ -0,0 +1,10 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hi Alice.')
+elif age < 20:
+    print('You are too young.')
+elif age > 2000:
+    print('You are too old.')
+elif age > 100:
+    print('Not a granny.')
